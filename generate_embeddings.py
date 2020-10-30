0f4ab8d298d034988d4005ecc20d2546719a3dcb
FrancoisPgm <francois.paugam@laposte.net>
2020-10-30 15:52:30 -0400

    use pybids

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -1,6 +1,8 @@
+import os
 import argparse
 from load_confounds import Params24
 from dypac import Dypac
+from bids import BIDSLayout
 import pickle
 import glob as glob
 import warnings
@@ -8,7 +10,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-from fetch_cneuromod import fetch_cneuromod
+#from fetch_cneuromod import fetch_cneuromod
 
 
 def dypac_generate(
@@ -53,105 +55,89 @@ def dypac_generate(
     return 0
 
 
-def get_prefix(files, clusters, states, batches, replications):
-
-    prefix = (
-        prefix
-        + "_cluster"
-        + str(clusters)
-        + "_states"
-        + str(states)
-        + "_batches"
-        + str(batches)
-        + "_reps"
-        + str(replications)
-    )
-
-    return prefix
+#def get_prefix(files, clusters, states, batches, replications):
+#
+#    prefix = (
+#        prefix
+#        + "_cluster"
+#        + str(clusters)
+#        + "_states"
+#        + str(states)
+#        + "_batches"
+#        + str(batches)
+#        + "_reps"
+#        + str(replications)
+#    )
+#
+#    return prefix
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-p", "--path", type=str, help="Specify the base path.")
-
-    parser.add_argument("-s", "--subject", type=str, help="Specify the subject.")
-
-    parser.add_argument(
-        "-ses", "--session", type=str, help="Specify the session you want."
-    )
-    parser.add_argument("-r", "--runs", type=str, help="Specify the run.")
-
+    parser.add_argument("-d", "--datasets", type=str, help="Specify the dataset.")
+    parser.add_argument("-der", "--derivatives", type=str, help="Specify the derivatives.", default="fmriprep-20.1.0/fmriprep")
+    parser.add_argument("--space", type=str, help="Specify the space of the bold files.", default="MNI152NLin2009cAsym")
+    parser.add_argument("-s", "--subjects", type=str, help="Specify the subject.")
+    parser.add_argument("-ses", "--sessions", type=str, help="Specify the session you want.", default="all")
+    parser.add_argument("-t", "--tasks", type=str, help="Specify the task.", default="all")
+    #parser.add_argument("-r", "--runs", type=str, help="Specify the run.", default="all")
     parser.add_argument("-n_clusters", type=int, help="Specify the number of clusters.")
-
     parser.add_argument("-n_states", type=int, help="Specify the number of states.")
-
-    parser.add_argument(
-        "-n_replications", type=int, help="Specify the number of replications."
-    )
-
+    parser.add_argument("-n_replications", type=int, help="Specify the number of replications.")
     parser.add_argument("-n_batch", type=int, help="Specify the number of batches.")
 
     args = parser.parse_args()
 
-    base_path = "/project/rrg-pbellec/datasets/cneuromod_new/movie10/derivatives/fmriprep-20.1.0/fmriprep/"
+#    base_path = "/project/rrg-pbellec/datasets/cneuromod_new/movie10/derivatives/fmriprep-20.1.0/fmriprep/"
 
-    subject = args.subject.split(",")
+    datasets = args.datasets.split(",")
+    subjects = args.subjects.split(",")
+    sessions = args.sessions.split(",")
+    tasks = args.tasks.split(",")
+    #runs = args.runs.split(",")
 
     ###Print the parameters.
 
-    print("\n\n\nSubject: ", subject)
-    session = args.session.split(",")
-    print("Session: ", session)
-    run = args.runs.split(",")
-    print("Run: ", run)
-    task = "bournesupremacy"
-    print("Task:", task)
-    print("\n")
+    print("\n\n\nDataset:{}\nDerivatives:{}\nSpace: {}\nSubjects:{}\nSessions:{}\nTasks:{}\n".format(
+        datasets, args.derivatives, args.space, subjects, sessions, tasks))
+
+    sessions = "" if sessions == ["all"] else sessions
+    tasks = "" if tasks == ["all"] else tasks
+    subjects = "" if subjects == ["all"] else subjects
+    #runs = "" if runs == ["all"] else runs
+    
+    ###Get the paths for the data
+    func = []
+    for dataset in datasets:
+        dataset_path = os.path.join(args.path, dataset)
+        derivatives_path = os.path.join(dataset_path, "derivatives", args.derivatives)
+        layout = BIDSLayout(dataset_path, derivatives=derivatives_path)
+        func = func + layout.get(subject=subjects, session=sessions, task=tasks, suffix="^bold$",
+                space=args.space, extension="nii.gz", scope="derivatives", regex_search=True, return_type="file")
 
 
-###Get the paths for the data
-func = fetch_cneuromod(subjects=subject,sessions=["all"], segments=["all"], runs=["run-1"],images=["bold"],datasets=["movie10"],tasks=["all"],list_out=True)["movie10"]
-
-func = func[0:10]
-
-
-##get the filename
-prefix = (
-    args.subject
-    + "_runs"
-    + str(len(func))
-    + "_cluster"
-    + str(args.n_clusters)
-    + "_states"
-    + str(args.n_states)
-    + "_batches"
-    + str(args.n_batch)
-    + "_reps"
-    + str(args.n_replications)
-)
-print(prefix)
+    ##get the filename
+    prefix = ("sub"+args.subjects+"_runs"+str(len(func))+"_cluster"+str(args.n_clusters)
+        +"_states"+str(args.n_states)+"_batches"+str(args.n_batch)+"_reps"
+        +str(args.n_replications))
+    print(prefix)
 
 
-##Get the path of the grey matter mask
-gm_path = (
-    base_path
-    + subject[0]
-    + "/anat/"
-    + subject[0]
-    + "_space-MNI152NLin2009cAsym_label-GM_probseg.nii.gz"
-)
+    ##Get the path of the grey matter mask
+    anat_path = os.path.join(args.path, "anat")
+    anat_derivatives_path = os.path.join(anat_path, "derivatives", args.derivatives)
+    anat_layout = BIDSLayout(anat_path, derivatives=anat_derivatives_path)
+    gm_path = anat_layout.get(subject=subjects, space=args.space, label="GM", scope="derivatives", return_type="file")[0]
+    print("gm_path = ", gm_path)
 
-print("gm_path = ", gm_path)
-
-
-dypac_generate(
-    func,
-    prefix,
-    gm_path,
-    n_clusters=args.n_clusters,
-    n_states=args.n_states,
-    n_batch=args.n_batch,
-    n_replications=args.n_replications,
-)
+    dypac_generate(
+        func,
+        prefix,
+        gm_path,
+        n_clusters=args.n_clusters,
+        n_states=args.n_states,
+        n_batch=args.n_batch,
+        n_replications=args.n_replications,
+    )
