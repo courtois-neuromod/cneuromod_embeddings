f08945d5d6ab085a24671e953b0fb627728b44b2
Pierre Bellec <pierre.bellec@gmail.com>
2020-12-12 11:38:53 -0500

    Reflecting name change inside script.

--- repro_friends-s01.sh
+++ repro_friends-s01.sh
@@ -6,14 +6,14 @@ export PATH_DATA='/data/cisl/pbellec/cneuromod_embeddings/xp_202012'
 echo Input data located in $PATH_DATA
 echo Results will be saved in $PATH_RESULTS
 
-python friends-s01_reproducibility.py --fwhm 5 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 5 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 5 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 5 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 5 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 8 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 8 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 8 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 8 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python friends-s01_reproducibility.py --fwhm 8 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 5 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 5 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 5 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 5 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 5 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 8 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 8 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 8 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 8 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
+python repro_friends-s01.py --fwhm 8 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
 
