9c16a318fe0a84ab356ecc4d926d965459ebe61f
pbellec <pierre.bellec@gmail.com>
2020-12-17 14:13:36 -0500

    Turning the generation of cortical and subcortical masks into a module.

---
+++ cortical_segmentation.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+import nibabel as nb
+
+from nilearn import image
+from nilearn import datasets
+
+def _load_aal():
+    aal = datasets.fetch_atlas_aal()
+    img_aal = nb.load(aal.maps)
+    mtx_aal = img_aal.get_fdata()
+    list_val = np.unique(mtx_aal)
+    return img_aal, mtx_aal, list_val
+
+
+def _expand_parcels(img_aal, mtx_aal, list_val):
+    all_val = np.zeros(np.append(mtx_aal.shape, list_val.shape[0]))
+    for ind, val in enumerate(list_val):
+        if ind > 0:
+            parcel = mtx_aal == val
+            img_parcel = image.new_img_like(img_aal, parcel)
+            img_parcel = image.smooth_img(img_parcel, fwhm=10)
+            all_val[:, :, :, ind] = img_parcel.get_fdata()
+    return all_val
+
+
+def cortical_segmentation():
+    """
+    Generate loose binary mask of the cortex, cerebellum, and central
+    structures (thalami, basal ganglia).
+    """
+    img_aal, mtx_aal, list_val = _load_aal()
+    all_val = _expand_parcels(img_aal, mtx_aal, list_val)
+    mtx_extra = list_val_full[np.argmax(all_val, axis=3)]
+    mask_central = (mtx_extra>=7000) & (mtx_extra<8000)
+    mask_cerebellum = mtx_extra >= 9000
+    mask_cortex = (mtx_extra > 0) & ~mask_central & ~mask_cerebellum
+    img = dict.fromkeys(['central', 'cerebellum', 'cortex'])
+    img['central'] = image.new_img_like(img_aal, mask_central)
+    img['cerebellum'] = image.new_img_like(img_aal, mask_cerebellum)
+    img['cortex'] = image.new_img_like(img_aal, mask_cortex)
