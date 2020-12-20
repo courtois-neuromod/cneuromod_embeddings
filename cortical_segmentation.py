688c57a66ea1072268405811dfb91ada12f29a7b
Pierre Bellec <pierre.bellec@gmail.com>
2020-12-20 11:42:25 -0500

    More prep work for the r2 experiments.

--- cortical_segmentation.py
+++ cortical_segmentation.py
@@ -5,6 +5,7 @@ import nibabel as nb
 from nilearn import image
 from nilearn import datasets
 
+
 def _load_aal():
     aal = datasets.fetch_atlas_aal()
     img_aal = nb.load(aal.maps)
@@ -24,7 +25,12 @@ def _expand_parcels(img_aal, mtx_aal, list_val):
     return all_val
 
 
-def cortical_segmentation():
+def _resample_segmentation(source_img, target_img):
+    img_r = image.resample_to_img(source_img, target_img, interpolation="nearest")
+    return img_r
+
+
+def cortical_segmentation(target_img):
     """
     Generate loose binary mask of the cortex, cerebellum, and central
     structures (thalami, basal ganglia).
@@ -32,11 +38,17 @@ def cortical_segmentation():
     img_aal, mtx_aal, list_val = _load_aal()
     all_val = _expand_parcels(img_aal, mtx_aal, list_val)
     mtx_extra = list_val[np.argmax(all_val, axis=3)]
-    mask_central = (mtx_extra>=7000) & (mtx_extra<8000)
+    mask_central = (mtx_extra >= 7000) & (mtx_extra < 8000)
     mask_cerebellum = mtx_extra >= 9000
     mask_cortex = (mtx_extra > 0) & ~mask_central & ~mask_cerebellum
-    img = dict.fromkeys(['central', 'cerebellum', 'cortex'])
-    img['central'] = image.new_img_like(img_aal, mask_central)
-    img['cerebellum'] = image.new_img_like(img_aal, mask_cerebellum)
-    img['cortex'] = image.new_img_like(img_aal, mask_cortex)
+    img = dict.fromkeys(["central", "cerebellum", "cortex"])
+    img["central"] = _resample_segmentation(
+        image.new_img_like(img_aal, mask_central), target_img
+    )
+    img["cerebellum"] = _resample_segmentation(
+        image.new_img_like(img_aal, mask_cerebellum), target_img
+    )
+    img["cortex"] = _resample_segmentation(
+        image.new_img_like(img_aal, mask_cortex), target_img
+    )
     return img
---
+++ r2_friends-s01.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import os
+import pickle
+import argparse
+import numpy as np
+from dypac_utils import match_components, subject_keys
+
+
+root_data = '/data/cisl/pbellec/cneuromod_embeddings/xp_202012/'
+
+
+def save_matx(match_matx, path_results, fwhm, cluster, state):
+    file_save = os.path.join(path_results, f'Rmatch_fwhm-{fwhm}_cluster-{cluster}_state-{state}.p')
+    print(file_save)
+    pickle.dump(match_matx, open(file_save, 'wb'))
+    
+
+def reproducibility(n_subject=6, fwhm=5, cluster=300, state=900):
+    list_subject = subject_keys(n_subject)
+    match_mtx = dict.fromkeys(list_subject)
+    for sub in list_subject:
+        match_mtx[sub] = dict.fromkeys(list_subject)
+
+    for ind_sub1 in range(n_subject):
+        sub1 = list_subject[ind_sub1]
+        for ind_sub2 in range(ind_sub1, n_subject):
+            sub2 = list_subject[ind_sub2]
+            print(f'matching parcels - {sub1} with {sub2}')
+            R = match_components(sub1=sub1, sub2=sub2, root_data=root_data, fwhm=fwhm, cluster=cluster, state=state)
+            match_mtx[sub1][sub2] = R
+            match_mtx[sub2][sub1] = R.transpose()
+    return match_mtx
+
+
+def main(args):
+    match_mtx = reproducibility(n_subject=6, fwhm=args.fwhm, cluster=args.cluster, state=args.state)
+    save_matx(match_mtx, args.path_results, args.fwhm, args.cluster, args.state)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_parcels', help='Full path to the parcels.')
+    parser.add_argument('path_results', help='Path to store the results.')
+    parser.add_argument('--fwhm', type=int, help='smoothing parameter.')
+    parser.add_argument('--cluster', type=int, help='number of clusters.')
+    parser.add_argument('--state', type=int, help='number of states.')
+    args = parser.parse_args()
+    main(args) 
