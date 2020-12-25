f5d7ff1123676498b199cbb9b5bf86d6a7a95e97
pbellec <pierre.bellec@gmail.com>
2020-12-25 11:56:38 -0500

    repro_summary should be ready.

--- cneuromod_embeddings/repro_summary.py
+++ cneuromod_embeddings/repro_summary.py
@@ -9,53 +9,44 @@ from cneuromod_embeddings.dypac_utils import dypac_params, subject_keys, key_par
 
 root_data = '/data/cisl/pbellec/cneuromod_embeddings/xp_202012/'
 
-    
-def key_repro(params):
-    list_keys = np.array([])
-    for fwhm in params['fwhm']:
-        for ind, cluster in enumerate(params['cluster']):
-            state = params['state'][ind]
-            key = f'fwhm-{fwhm}_cluster-{cluster}_state-{state}'
-            list_keys = np.append(list_keys, key)
-    return list_keys
+def load_repro(root_data, fwhm, cluster, state):
+    key = f'fwhm-{fwhm}_cluster-{cluster}_state-{state}'
+    print(key)
+    file_repro = os.path.join(root_data, f'Rmatch_{key}.p')
+    R = pickle.load(open(file_repro, 'rb'))
+    return R
 
 
-def load_repro(root_data):
+def summary_repro(root_data):
     val = np.array([])
-    all_fwhm = np.array([])
     all_label = np.array([])
+    all_fwhm = np.array([])
     type_comp = np.array([])
-    skip_fwhm = False
     params = dypac_params()
     list_subject = subject_keys(n_subject=6)
     for fwhm in params['fwhm']:
         for ind, cluster in enumerate(params['cluster']):
             state = params['state'][ind]
-            key = f'fwhm-{fwhm}_cluster-{cluster}_state-{state}'
-
-    for key in list_keys:
-        print(key)
-        file_repro = os.path.join(root_data, f'Rmatch_{key}.p')
-        R = pickle.load(open(file_repro, 'rb'))
-        for sub1 in list_subject:
-            for sub2 in list_subject:
-                match_val = np.max(R[sub1][sub2], axis=1)
-                val = np.append(val, match_val)
-                label = re.search('.......(.+)', key).group(1)
-                all_label = np.append(all_label, np.repeat(label, match_val.shape[0]))
-                all_fwhm = np.append(all_fwhm, 
-                if sub1 == sub2:
-                    type_comp = np.append(type_comp, np.repeat('intra', match_val.shape[0]))
-                else:
-                    type_comp = np.append(type_comp, np.repeat('inter', match_val.shape[0]))
-    return pd.DataFrame({'spatial_r': val, 'params': all_label, 'type_comp': type_comp})
+            R = load_repro(root_data, fwhm, cluster, state)
+            for sub1 in list_subject:
+                for sub2 in list_subject:
+                    match_val = np.max(R[sub1][sub2], axis=1
+                    val = np.append(val, match_val)
+                    label = f'cluster-{cluster}_state-{state}'
+                    all_label = np.append(all_label, np.repeat(label, match_val.shape[0]))
+                    all_fwhm = np.append(all_fwhm, np.repeat(fwhm, match_val.shape[0]))
+                    if sub1 == sub2:
+                        type_comp = np.append(type_comp, np.repeat('intra', match_val.shape[0]))
+                    else:
+                        type_comp = np.append(type_comp, np.repeat('inter', match_val.shape[0]))
+    return pd.DataFrame({'spatial_r': val, 'params': all_label, 'fwhm': all_fwhm, 'type_comp': type_comp})
 
 
 def save_matx(match_matx, path_results, fwhm, cluster, state):
     file_save = os.path.join(path_results, f'Rmatch_fwhm-{fwhm}_cluster-{cluster}_state-{state}.p')
     print(file_save)
     pickle.dump(match_matx, open(file_save, 'wb'))
-    
+
 
 def reproducibility(n_subject=6, fwhm=5, cluster=300, state=900):
     list_subject = subject_keys(n_subject)
@@ -86,4 +77,4 @@ if __name__ == '__main__':
     parser.add_argument('--cluster', type=int, help='number of clusters.')
     parser.add_argument('--state', type=int, help='number of states.')
     args = parser.parse_args()
-    main(args) 
+    main(args)
