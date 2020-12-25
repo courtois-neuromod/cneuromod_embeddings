1984146092f38f75432ebd0952993d9379963663
pbellec <pierre.bellec@gmail.com>
2020-12-25 10:57:03 -0500

    Started working the reproducibility part in the book.

--- cneuromod_embeddings/repro_friends-s01.sh
+++ cneuromod_embeddings/repro_friends-s01.sh
@@ -1,19 +1,14 @@
 #/bin/bash
-
-export PATH_RESULTS='/data/cisl/pbellec/cneuromod_embeddings/xp_202012/repro_friends-s01'
-export PATH_DATA='/data/cisl/pbellec/cneuromod_embeddings/xp_202012'
+myInvocation="$(printf %q "$BASH_SOURCE")$((($#)) && printf ' %q' "$@")"
+export FWHM=$2
+export PATH_RESULTS=$1'/repro_friends-s01'
+export PATH_DATA=$1
 
 echo Input data located in $PATH_DATA
 echo Results will be saved in $PATH_RESULTS
 
-python repro_friends-s01.py --fwhm 5 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 5 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 5 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 5 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 5 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 8 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 8 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 8 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 8 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python repro_friends-s01.py --fwhm 8 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
-
+python repro_summary.py --fwhm $FWHM --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm $FWHM --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm $FWHM --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm $FWHM --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm $FWHM --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
