94cbd950fc380f98adc4e3e099cc9f9f11f5091a
pbellec <pierre.bellec@gmail.com>
2020-12-25 23:07:06 -0500

    Switched back to have all FWHM in the same script.

--- cneuromod_embeddings/repro_friends-s01.sh
+++ cneuromod_embeddings/repro_friends-s01.sh
@@ -1,14 +1,18 @@
 #/bin/bash
 myInvocation="$(printf %q "$BASH_SOURCE")$((($#)) && printf ' %q' "$@")"
-export FWHM=$2
-export PATH_RESULTS=$1'/repro_friends-s01_'$FWHM'/' 
+export PATH_RESULTS=$1'/repro_friends-s01/'
 export PATH_DATA=$1
 
 echo Input data located in $PATH_DATA
 echo Results will be saved in $PATH_RESULTS
 
-python repro_summary.py --fwhm $FWHM --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm $FWHM --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm $FWHM --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm $FWHM --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm $FWHM --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
