c0ee4038938c92e191dbf486c55dc9147614e92e
FrancoisPgm <francois.paugam@laposte.net>
2021-01-08 15:14:26 -0500

    small refactoring

--- compute_r2.py
+++ compute_r2.py
@@ -1,16 +1,22 @@
 import h5py
 from bids import BIDSLayout
 import os
+import numpy as np
 import argparse
 from load_confounds import Params36
 import pickle
 import dypac_masker
 from nilearn import datasets
 
+
 def compute_r2(model, file_paths, confounds, prefix, out_dir, tag=None,
                group_name=None): 
 
-    tag = "_" + tag if tag else ""
+    if tag:
+        if tag[0] != "_":
+            tag = "_" + tag
+    else:
+        tag = ""
     out_path = os.path.join(out_dir, prefix + tag + "_r2_scores.hdf5")
     
     for i in range(len(file_paths)):
@@ -26,27 +32,33 @@ def compute_r2(model, file_paths, confounds, prefix, out_dir, tag=None,
                     grp = grp[group]
         dset = grp.create_dataset(os.path.split(file_paths[i])[1], data=r2)
         h5_file.close()
-    #os.system("setfacl -m g:rrg-pbellec:rwx {}".format(out_path))
 
 
 if __name__ == "__main__":
 
-    print("hello")
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", type=str, help="Path to the dypac model.")
     parser.add_argument("-d", "--dataset", type=str, help="Path to the dataset.")
-    parser.add_argument("-b", "--bids_folder", type=str, help="Path to the bids folder of the saved layout of the dataset.", default=None)
-    parser.add_argument("-der", "--derivatives", type=str, help="Specify the derivatives", default="fmriprep-20.1.0/fmriprep")
-    parser.add_argument("--space", type=str, help="Specify the space of the bold files.", default="MNI152NLin2009cAsym")
-    parser.add_argument("-s", "--subjects", type=str, help="Specify the subject.", default="all")
-    parser.add_argument("-ses", "--sessions", type=str, help="Specify the session you want.", default="all")
-    parser.add_argument("-t", "--tasks", type=str, help="Specify the task.", default="all")
-    parser.add_argument("-g", "--group", type=str, help="Specify the name for the hdf5 file group (no group if the name is None).", default=None)
-    parser.add_argument("-tag", type=str, help="Tag to append at the end of the file name.", default=None)
-    parser.add_argument("-p", "--prefix", type=str,
-            help="Prefix to use for the output file name, if None the file name of the model is used.", default=None)
-    parser.add_argument("-o", "--out_dir", type=str,
-            help="Path to the directory where to save the results. If None, the directory of the model is used.", default=None)
+    parser.add_argument("-b", "--bids_folder", type=str,
+            help="Path to the bids folder of the saved layout of the dataset.", default=None)
+    parser.add_argument("-der", "--derivatives", type=str, help="Specify the derivatives",
+            default="fmriprep-20.1.0/fmriprep")
+    parser.add_argument("--space", type=str, help="Specify the space of the bold files.",
+            default="MNI152NLin2009cAsym")
+    parser.add_argument("-s", "--subjects", type=str, default="all",
+            help="Specify the subjects. String with coma separated subjects (regex accepted), or 'all'.")
+    parser.add_argument("-ses", "--sessions", type=str, defaul="all",
+            help="Specify the sessions. String with coma separated sessions (regex accepted), or 'all'.")
+    parser.add_argument("-t", "--tasks", type=str, default="all",
+            help="Specify the task.i String with coma separated tasks (regex accepted), or 'all'.")
+    parser.add_argument("-g", "--group", type=str, default=None,
+            help="Specify the name for the hdf5 file group (no group if None).")
+    parser.add_argument("-tag", type=str, default=None,
+            help="Tag to append at the end of the output files names.")
+    parser.add_argument("-p", "--prefix", type=str, default=None,
+            help="Prefix to use for the output file name, if None the file name of the model is used.")
+    parser.add_argument("-o", "--out_dir", type=str, default=None,
+            help="Path to the directory where to save the results. If None, the directory of the model is used.")
     parser.add_argument("-a", "--atlas", type=str, help="Name of atlas.", default=None)
 
     args = parser.parse_args()
@@ -61,12 +73,13 @@ if __name__ == "__main__":
 
     prefix = args.prefix if args.prefix else os.path.splitext(os.path.split(args.model)[1])[0]
     out_dir = args.out_dir if args.out_dir else os.path.split(args.model)[0] 
+    tag = "_" + args.tag if args.tag else ""
 
     derivatives_path = os.path.join(args.dataset, "derivatives", args.derivatives)
-    print ("deriv", derivatives_path)
     layout = BIDSLayout(args.dataset, derivatives=derivatives_path, database_path=args.bids_folder)
     with open(args.model, "rb") as pickle_in:
         model = pickle.load(pickle_in)
+
     if args.atlas:
         if args.atlas == "schaefer":
             atlas_path = "data/schaefer_2018/Schaefer2018_400Parcels_7Networks_order_FSLMNI152_1mm.nii.gz"
@@ -78,19 +91,38 @@ if __name__ == "__main__":
             scale = args.atlas[4:]
             atlas_path = "data/MIST/Parcellations/MIST_{}.nii.gz".format(scale)
             model = dypac_masker.LabelsMasker(model=model, labels=atlas_path)
+        elif "difumo" in args.atlas:
+            scale = args.atlas[6:]
+            atlas_path = "data/DIFUMO/DIFUMO_{}.nii.gz".format(scale)
+            model = dypac_masker.MapsMasker(model=model, maps=atlas_path)
+        elif args.atlas == "dypac_clean":
+            components = np.array(model.components_.todense())
+            components[components<0.1] = 0
+            mask = list(np.sum(components, axis=1)>0)
+            components = components[mask, :]
+            maps = model.masker_.inverse_transform(components)
+            model = dypac_masker.MapsMasker(model=model, maps=maps)
+            tag = "_clean"
+        elif args.atlas == "dypac_clean_multi_fwhm":
+            model_fwhm_5 = model
+            path_model_fwhm_8 = args.model.replace("fwhm-5", "fwhm-8")
+            with open(path_model_fwhm_8, "rb") as pickle_in:
+                model_fwhm_8 = pickle.load(pickle_in)
+            components = np.array(model_fwhm_8.components_.todense())
+            components[components<0.1] = 0
+            mask = list(np.sum(components, axis=1)>0)
+            components = components[mask, :]
+            maps = model_fwhm_5.masker_.inverse_transform(components)
+            model = dypac_masker.MapsMasker(model=model_fwhm_5, maps=maps)
+            tag = "_clean_multi_fwhm"
+        out_path_model = os.path.join(out_dir, prefix+tag+".pickle")
+        pickle.dump(model, open(out_path_model, "wb"))
 
-    if len(subjects) > 1:
-        for sub in subjects:
-            file_paths = layout.get(subject=sub, session=sessions, task=tasks, suffix="^bold$", space=args.space,
-                                    extension="nii.gz", scope="derivatives", regex_search=True, return_type="file")
-            confounds = Params36().load(file_paths)
-            compute_r2(model, file_paths, confounds, prefix, out_dir, args.tag, args.group+"/sub-"+sub)
-            print("done with", sub)
-    else:
-        file_paths = layout.get(subject=subjects, session=sessions, task=tasks, suffix="^bold$", space=args.space,
+    for sub in subjects:
+        file_paths = layout.get(subject=sub, session=sessions, task=tasks, suffix="^bold$", space=args.space,
                                 extension="nii.gz", scope="derivatives", regex_search=True, return_type="file")
-        print(file_paths)
-        print("---------------------------------")
         confounds = Params36().load(file_paths)
-        compute_r2(model, file_paths, confounds, prefix, out_dir, args.tag, args.group)
+        group = args.group+"/sub-"+sub if len(subjects) > 1 else args.group
+        compute_r2(model, file_paths, confounds, prefix, out_dir, tag, group)
+        print("done with", sub)
 
--- generate_embeddings.py
+++ generate_embeddings.py
@@ -13,14 +13,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-
 def dypac_generate(
-    func, conf, gm_path, n_clusters=10, n_states=5, n_batch=1, n_replications=40, fwhm=8
+    data_paths, conf, gm_path, n_clusters=10, n_states=5, n_batch=1, n_replications=40, fwhm=8
 ):
     print("Settings")
     print(
-        "runs = %s, n_clusters = %s, n_states = %s, n_batches = %s, n_replcations = %s \n"
-        % (len(func), n_clusters, n_states, n_batch, n_replications)
+        "files = %s, n_clusters = %s, n_states = %s, n_batches = %s, n_replcations = %s, fwhm = %s\n"
+        % (len(data_paths), n_clusters, n_states, n_batch, n_replications, fwhm)
     )
 
     model = Dypac(
@@ -37,22 +36,31 @@ def dypac_generate(
         smoothing_fwhm=fwhm,
         standardize=True,
     )
-    print("the length of confounds list", len(conf))
 
-    model.fit(func, confounds=conf)
+    model.fit(data_paths, confounds=conf)
     return model
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", type=str, help="Specify the base path.", default="data/cneuromod")
-    parser.add_argument("-d", "--datasets", type=str, help="Specify the dataset.")
-    parser.add_argument("-der", "--derivatives", type=str, help="Specify the derivatives.", default="fmriprep-20.1.0/fmriprep")
-    parser.add_argument("--space", type=str, help="Specify the space of the bold files.", default="MNI152NLin2009cAsym")
-    parser.add_argument("-s", "--subjects", type=str, help="Specify the subject.")
-    parser.add_argument("-ses", "--sessions", type=str, help="Specify the session you want.", default="all")
-    parser.add_argument("-t", "--tasks", type=str, help="Specify the task.", default="all")
-    parser.add_argument("--val", type=str, help="Specify the task for validation r2.")
+    parser.add_argument("-p", "--path", type=str,
+            help="Specify the base path of the cneuromod dataset.", default="data/cneuromod")
+    parser.add_argument("-d", "--datasets", type=str,
+            help="Specify the dataset (e.g. 'friends', 'movie10').")
+    parser.add_argument("-der", "--derivatives", type=str, help="Specify the derivatives.",
+            default="fmriprep-20.1.0/fmriprep")
+    parser.add_argument("--space", type=str, help="Specify the space of the bold files.",
+            default="MNI152NLin2009cAsym")
+    parser.add_argument("-s", "--subjects", type=str,
+            help="Specify the subjects. String with coma separated subjects (regex accepted), or 'all'.")
+    parser.add_argument("-ses", "--sessions", type=str,
+            help="Specify the sessions. String with coma separated sesions (regex accepted), or 'all'.",
+            default="all")
+    parser.add_argument("-t", "--tasks", type=str,
+            help="Specify the task. String with coma separated tasks (regex accepted), or 'all'.",
+            default="all")
+    parser.add_argument("--val", type=str,
+            help="Specify the tasks for validation r2. String with coma sperated tasks (regex accepted).")
     parser.add_argument("-n_clusters", type=int, help="Specify the number of clusters.")
     parser.add_argument("-n_states", type=int, help="Specify the number of states.")
     parser.add_argument("-n_replications", type=int, help="Specify the number of replications.")
@@ -67,16 +75,16 @@ if __name__ == "__main__":
     tasks = args.tasks.split(",")
     val_tasks = args.val.split(",")
 
-    ###Print the parameters.
-
+    # Print the parameters.
     print("\n\n\nDataset:{}\nDerivatives:{}\nSpace: {}\nSubjects:{}\nSessions:{}\nTasks:{}\nVal:{}\n".format(
         datasets, args.derivatives, args.space, subjects, sessions, tasks, val_tasks))
 
+    # replace "all" value by empty strings for the regex filters
     sessions = "" if sessions == ["all"] else sessions
     tasks = "" if tasks == ["all"] else tasks
     subjects = "" if subjects == ["all"] else subjects
     
-    ###Get the paths for the data
+    # Get the paths for the data
     tng_paths = []
     val_paths = []
     for dataset in datasets:
@@ -88,16 +96,13 @@ if __name__ == "__main__":
         val_paths = val_paths + layout.get(subject=subjects, session=sessions, task=val_tasks, suffix="^bold$",
                 space=args.space, extension="nii.gz", scope="derivatives", regex_search=True, return_type="file")
 
-    print("Using {} files:".format(len(tng_paths)))
-    print("--------------TNG--------------")
-    print(tng_paths)
-    print("--------------VAL--------------")
-    print(val_paths)
+    print("Using {} files for fitting.".format(len(tng_paths)))
+    print("Using {} files for validation.".format(len(val_paths)))
 
     conf = Params36().load(tng_paths)
     val_conf = Params36().load(val_paths)
 
-    ##get the filename
+    # make the prefix to use for the output files names
     task_str = args.tasks
     if "e.[02468]" in task_str:
         task_str = task_str.replace("e.[02468]", "even")
@@ -107,7 +112,7 @@ if __name__ == "__main__":
         +"_states-"+str(args.n_states)+"_batches-"+str(args.n_batch)+"_reps-"
         +str(args.n_replications)+"_fwhm-"+str(args.fwhm))
     prefix = "sub-"+args.subjects+"_"+out_dir_name
-    print(prefix)
+    print("prefix = ", prefix)
 
 
     ##Get the path of the grey matter mask
