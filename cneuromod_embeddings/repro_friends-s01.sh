b1accc1ced6531acf54328d154733feba5efde41
Pierre Bellec <pierre.bellec@gmail.com>
2021-01-09 00:18:46 -0500

    WIP on repro.

--- cneuromod_embeddings/repro_friends-s01.sh
+++ cneuromod_embeddings/repro_friends-s01.sh
@@ -2,17 +2,19 @@
 myInvocation="$(printf %q "$BASH_SOURCE")$((($#)) && printf ' %q' "$@")"
 export PATH_RESULTS=$1'/repro_friends-s01/'
 export PATH_DATA=$1
+export XP_TYPE=$2
 
+echo Experiment type: $XP_TYPE
 echo Input data located in $PATH_DATA
 echo Results will be saved in $PATH_RESULTS
 
-python repro_summary.py --fwhm 5 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 5 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 5 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 5 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 5 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 8 --cluster 20  --state 60  $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 8 --cluster 20  --state 120 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 8 --cluster 50  --state 150 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 8 --cluster 50  --state 300 $PATH_DATA $PATH_RESULTS &
-python repro_summary.py --fwhm 8 --cluster 300 --state 900 $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 20  --state 60  --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 20  --state 120 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 50  --state 150 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 50  --state 300 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 5 --cluster 300 --state 900 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 20  --state 60  --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 20  --state 120 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 50  --state 150 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 50  --state 300 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
+python repro_summary.py --fwhm 8 --cluster 300 --state 900 --xp_type $XP_TYPE $PATH_DATA $PATH_RESULTS &
